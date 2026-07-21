5e67645fffa144d963f41a4dc07f14ec845dd196
Olivia Moody <owymoody@gmail.com>
2026-07-21 19:51:37 -0400

    fix: added missing length parameter. feat: outlined functions to implement

--- LinkedList.js
+++ LinkedList.js
@@ -3,5 +3,17 @@ class LinkedList {
     const newNode = new Node(value);
     this.head = newNode;
     this.tail = this.head;
+    this.length = 1;
   }
+
+  //TODO: push function (given a value, add a new node to end of list)
+  //TODO: pop function (removes and returns node at end of list) //remember to consider edge cases: what if there's only one node?
+  //TODO: unshift function (given a value, add a new node to begining of list)
+  //TODO: shift function (removes and returns node at beginning of list)
+  //TODO: get function (given an index, return the node at that index)
+  //TODO: set function (given an index and a value, set the node at that index to the value)
+  //TODO: print function (prints the entire linked list)
+  //TODO: insert function (given an index and a value, insert a new node at that index)
+  //TODO: remove function (given an index, remove the node at that index)
+  //TODO: reverse function (in place reversal)
 }
