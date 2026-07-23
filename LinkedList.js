890e6bfa79ede08a40ed87c15893f4a035372f1d
Olivia Moody <owymoody@gmail.com>
2026-07-23 00:55:15 -0400

    feat: implemented getter, setter, print, and insert function

--- LinkedList.js
+++ LinkedList.js
@@ -49,7 +49,7 @@ class LinkedList {
     this.length++;
   }
 
-  //TODO: shift function (removes and returns node at beginning of list)
+  //DONE: shift function (removes and returns node at beginning of list)
   shift() {
     if(this.length === 0) return console.error("Linked List is already empty");
     let headVal = this.head.value;
@@ -58,9 +58,11 @@ class LinkedList {
     return headVal;
   }
 
-  //TODO: get function (given an index, return the node at that index)
+  //DONE: get function (given an index, return the node at that index)
   get(index) {
-    if(index < 0 || index > this.length) console.error("Index is out of range.");
+    if(index < 0 || index > this.length) return console.error("Index is out of range.");
+    if(this.head === null) return console.error("Linked list is empty");
+    if(this.length === 1) return this.head.value;
     let curr = this.head;
     for(let i = 0; i<index; i++) {
       curr=curr.next;
@@ -68,24 +70,55 @@ class LinkedList {
     return curr.value;
   }
 
-  //TODO: set function (given an index and a value, set the node at that index to the value)
-  //TODO: print function (prints the entire linked list)
-  //TODO: insert function (given an index and a value, insert a new node at that index)
+  //DONE: set function (given an index and a value, set the node at that index to the value)
+  set(index, value) {
+    if(index < 0 || index > this.length) return console.error("Index is out of range.");
+    let curr = this.head;
+    for(let i = 0; i<index; i++) {
+      curr = curr.next;
+    }
+    curr.value = value;
+  }
+
+  //DONE: print function (prints the entire linked list)
+  printLL() {
+    let curr = this.head;
+    while(curr !== null) {
+      console.log(curr.value);
+      curr = curr.next;
+    }
+  }
+
+  //NEEDS REVIEW: insert function (given an index and a value, insert a new node at that index)
+  insert(index, value) {
+    if(index < 0 || index > this.length) return console.error("Index is out of scope");
+    if(index === 0) this.unshift(value);
+    if(index === this.length) this.push(value);
+    const newNode = new Node(value);
+    let curr = this.head;
+    for(let i = 0; i < index; i++) {
+      curr = curr.next;
+    }
+    newNode.next = curr.next;
+    curr.next = newNode;
+  }
+
   //TODO: remove function (given an index, remove the node at that index)
   //TODO: reverse function (in place reversal)
 }
 
 function test() {
   const myLL = new LinkedList(4);
-  console.log(myLL);
+  myLL.printLL();
   myLL.push(5);
-  console.log(myLL);
+  myLL.printLL();
   console.log("Should return 5", myLL.pop());
   myLL.unshift(3);
-  console.log(myLL);
+  myLL.printLL();
   console.log("Should return 3", myLL.shift());
   console.log(myLL.get(0));
-  console.log(myLL.get(13));
+  myLL.set(0, 2);
+  myLL.printLL();
 }
 
 test();
